f7c359af840e19596d666a49271417690a80e7f3
David Ramage <dramage@github.io>
2018-01-14 18:07:42 -0800

    add temp index value for snmp

--- init.lua
+++ init.lua
@@ -1,9 +1,24 @@
-
-SSID = "MY_SSID"
-WPA_KEY = "MY_KEY"
-
+WIFI_CONFIG = {}
+WIFI_CONFIG.ssid = "SSID_NAME"
+WIFI_CONFIG.pwd = "WPA2_KEY"
 MAX_CONNECT_ATTEMPTS = 100
 CONNECT_ATTEMPT = 0
+
+PIN_18B20 = 4 
+TEMPERATURE = 0.0
+
+function make_temperature_integer_response(comm_len, community, request_id)
+    print("comm_len" .. comm_len)
+    print("community" .. community)
+    print("request id" .. request_id)
+    community_len = string.len(community)
+    pack_str = '> I1 I1 I1 I1 I1 I1 I1 c' .. comm_len .. 'I1 I1 I1 I1 I4 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1 I1'
+    return struct.pack(pack_str, 0x30, 0x29 + community_len, 0x02, 0x01, 0x00, 0x04, community_len,
+    community, 0xA2, 0x22, 0x02, 0x04, request_id, 0x02, 0x01, 0x00, 0x02, 0x01,
+    0x00, 0x30, 0x14, 0x30, 0x12, 0x06, 0x0d, 0x2b, 0x06, 0x01, 0x04, 0x01, 0x8f, 0x65, 0x0d, 0x10,
+    0x02, 0x01, 0x01, 0x0d, 0x02, 0x01, 0x0d, 0x01, 0x0d)
+
+end
 
 function make_get_sysname_response(sysname, comm_len, community, request_id, varbind, obj_hi, obj_lo)
     sysname_len = string.len(sysname)
@@ -44,6 +59,9 @@ function start_server()
         -- 1.3.6.1.2.1.1.6.0 - syslocation
         return_str = make_get_sysname_response('someplace good', comm_len, comm_string, req_id, varbind, obj_hi, obj_lo)
         s:send(port, ip, return_str)
+      elseif (obj_hi == 0x2b060104 and obj_lo == 0x18f650d) then
+        return_str = make_temperature_integer_response(comm_len, comm_string, req_id)
+        s:send(port, ip, return_str)
       end
 	end)
 end
@@ -52,6 +70,7 @@ function check_connection()
     if( CONNECT_ATTEMPT > MAX_CONNECT_ATTEMPTS) then
         print("failed to connect to wifi!")
     else
+        CONNECT_ATTEMPT = CONNECT_ATTEMPT + 1
         ip_address = wifi.sta.getip()
         if ((ip_address ~= nil) and (ip_address ~= '0.0.0.0')) then
             print(ip_address)
@@ -65,12 +84,57 @@ function check_connection()
     
 end
 
+function GET_18B20_TEMP(addr)
+    ow.reset(PIN_18B20)
+    ow.select(PIN_18B20, addr)
+    ow.write(PIN_18B20, 0x44, 1)
+    tmr.delay(1000000)
+    present = ow.reset(PIN_18B20)
+    ow.select(PIN_18B20, addr)
+    ow.write(PIN_18B20,0xBE,1)
+    print("P="..present)  
+    data = nil
+    data = string.char(ow.read(PIN_18B20))
+    for i = 1, 8 do
+        data = data .. string.char(ow.read(PIN_18B20))
+    end
+    print(data:byte(1,9))
+    crc = ow.crc8(string.sub(data,1,8))
+    print("CRC="..crc)
+    if crc == data:byte(9) then
+       t = (data:byte(1) + data:byte(2) * 256) * 625
+       t1 = t / 10000
+       print("Temperature="..t1.." Centigrade")
+    end
+end
+
+function INIT_18B20()
+    ow.setup(PIN_18B20)
+    count = 0
+    repeat
+        count = count + 1
+        addr = ow.reset_search(PIN_18B20)
+        addr = ow.search(PIN_18B20)
+        tmr.wdclr()
+    until (addr ~= nil) or (count > 100)
+    if addr == nil then
+        print("No more addresses.")
+    else
+        print(addr:byte(1,8))
+    end
+    crc = ow.crc8(string.sub(addr,1,7))
+    if crc == addr:byte(8) then
+        print(string.format("0x%x, we have an 18B20, getting temp",addr:byte(1)))
+        GET_18B20_TEMP(addr)
+    end
+end
+
 print("Starting system")
 ip_address = wifi.sta.getip()
-
+INIT_18B20()
 if ((ip_address ~= nil) and (ip_address ~= '0.0.0.0')) then
     wifi.setmode(wifi.STATION)
-    wifi.sta.config(SSID, WPA_KEY)
+    wifi.sta.config(WIFI_CONFIG)
 else
     tmr.alarm(0,2500,0,check_connection) 
 end
